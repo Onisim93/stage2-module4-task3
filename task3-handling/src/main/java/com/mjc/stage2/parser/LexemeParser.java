50536012e3c77b3e79f63a93887abb3dd7872d03
onisim_albarov <onisim93@gmail.com>
2023-01-11 15:39:11 +0400

    completed stage2-module4-task3

--- task3-handling/src/main/java/com/mjc/stage2/parser/LexemeParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/LexemeParser.java
@@ -1,9 +1,17 @@
 package com.mjc.stage2.parser;
 
-public class LexemeParser {
-    private static final String LEXEME_REGEX = "\\s+";
-    private static final String WORD_REGEX = "\\w[\\w!=?():]+";
+import com.mjc.stage2.entity.AbstractTextComponent;
+import com.mjc.stage2.entity.TextComponent;
+import com.mjc.stage2.entity.TextComponentType;
 
-    // Write your code here!
+public class LexemeParser extends AbstractTextParser{
+    private static final String LEXEME_REGEX = "\\s+";
+    @Override
+    public void parse(AbstractTextComponent abstractTextComponent, String string) {
+        String[] lexemes = string.split(LEXEME_REGEX);
+        for (String l : lexemes) {
+            abstractTextComponent.add(new TextComponent(TextComponentType.WORD));
+        }
+    }
 
 }
